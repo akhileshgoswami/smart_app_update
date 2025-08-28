a2ec5d63772be67961e8fcc02eba612813473283
akhileshgoswami <akgileshgoswami@gmail.com>
2025-08-28 18:57:02 +0530

    updated code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,9 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
-import 'package:smart_app_update_flutter/src/app_update_manager.dart';
-import 'package:smart_app_update_flutter/src/ui/update_bottom_sheet.dart';
-import 'package:smart_app_update_flutter/smart_app_update_flutter.dart';
+import 'package:smart_app_update_flutter/smart_app_update.dart';
+
 
 void main() {
   runApp(const MyApp());
